6f9f481cdec7b8996237644e90ece05bd4267522
Nrupen Desai <nrupen.desai@yahoo.com>
2016-11-16 20:59:25 -0800

    code update for user.

--- backend/application/controllers/User.php
+++ backend/application/controllers/User.php
@@ -5,6 +5,10 @@ class User extends CI_Controller {
 		parent::__construct ();
 		$this->load->model ( 'User_model' );
 		$this->load->database ();
+		header('Access-Control-Allow-Origin: *');
+		header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+		header('Access-Control-Allow-Methods: GET, POST, PUT');
+		
 	}
 	public function index() {
 		$this->load->view ( 'welcome_message' );
