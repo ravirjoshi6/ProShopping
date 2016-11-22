b8be85a151a7a6e6c05b0cdbad65506a7b05c53e
admin <admin@MIHIR-PC>
2016-11-22 14:35:49 -0800

    Product details View changes

--- scripts/models/product.js
+++ scripts/models/product.js
@@ -3,10 +3,12 @@
         productid: "12",
         name: "GAP FUll SLEEV",
         desc: {
+            details:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam. Ut wisi enim ad minim veniam iriure dolor in hendrerit in vulputate velit esse molestie consequat.Product ",
             material:"bogus",
             color: "red",
             type: "FULL SLEEV",
-            brand: "GAP"
+            brand: "GAP",
+            gender:"male"
         },
         price: "12",
         img: "/images/a1.jpg",
