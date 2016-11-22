36141236f6985cb92645a7247bbe22ec85a45a1e
rrjoshi92 <rrjoshi92@gmail.com>
2016-11-22 12:41:04 -0800

    Hompage products added. imaged added. product list model created.

---
+++ scripts/models/productlist.js
@@ -0,0 +1,76 @@
+﻿var ProductList = function () {
+    var productlist = [{
+        productid: "1",
+        name: "Nike Red tee-shirt",
+        desc: "des 1",
+        price: "123",
+        img: "images/nike-red.jpg",
+        isactive: "Y",
+        size: "XL"
+    },
+    {
+        productid: "2",
+        name: "Nike Black tee shirt",
+        desc: "des 2",
+        price: "345",
+        img: "images/nike-black.jpg",
+        isactive: "Y",
+        size: "S"
+    },
+    {
+        productid: "3",
+        name: "Nike-Gray tee shirt",
+        desc: "des 3",
+        price: "456",
+        img: "images/nike-gray.jpg",
+        isactive: "Y",
+        size: "L"
+    },
+    {
+        productid: "4",
+        name: "Nike Blue tee shirt",
+        desc: "des 4",
+        price: "789",
+        img: "images/nike-blue.jpg",
+        isactive: "Y",
+        size: "M"
+    },
+    {
+        productid: "5",
+        name: "Nike Red tee-shirt",
+        desc: "des 1",
+        price: "123",
+        img: "images/nike-red.jpg",
+        isactive: "Y",
+        size: "XL"
+    },
+    {
+        productid: "6",
+        name: "Nike Black tee shirt",
+        desc: "des 2",
+        price: "345",
+        img: "images/nike-black.jpg",
+        isactive: "Y",
+        size: "S"
+    },
+    {
+        productid: "7",
+        name: "Nike-Gray tee shirt",
+        desc: "des 3",
+        price: "456",
+        img: "images/nike-gray.jpg",
+        isactive: "Y",
+        size: "L"
+    },
+    {
+        productid: "8",
+        name: "Nike Blue tee shirt",
+        desc: "des 4",
+        price: "789",
+        img: "images/nike-blue.jpg",
+        isactive: "Y",
+        size: "M"
+    }
+    ]
+    return productlist;
+}
