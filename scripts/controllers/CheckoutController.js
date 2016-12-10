d9de07310939b04d4b1f1e2493181f4a16caa2dd
admin <admin@MIHIR-PC>
2016-12-09 17:05:26 -0800

    Validation: CheckoutValidation.js added.

--- scripts/controllers/CheckoutController.js
+++ scripts/controllers/CheckoutController.js
@@ -4,5 +4,7 @@
     app.controller('CheckoutController', function ($scope, cart) {
         var cart = JSON.parse(sessionStorage.getItem('cart'));
         $scope.cart = cart;
+        var chk = new CheckoutValidtion();
+        
     });
 }());
---
+++ scripts/controllers/CheckoutValidation.js
@@ -0,0 +1,3 @@
+﻿var CheckoutValidtions = function () {
+    alert("validations");
+}
