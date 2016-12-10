38f54cce4c63dedb115ebca75ec58fff6fecb1a1
NRUPEN-DESAI\nrupe <nrupen92@gmail.com>
2016-12-10 00:29:13 -0800

    code for change host url

--- backend/application/config/config.php
+++ backend/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://proshop.my-labs.info/';
+$config['base_url'] = 'http://capstone.devview.info/';
 
 /*
 |--------------------------------------------------------------------------
